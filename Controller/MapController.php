149e3ae63db3feeeb96854b81cfa1ed41ab8619b
alexander.gansky <alexander.gansky@gmail.com>
2013-10-17 02:21:41 +0300

    map + controller

--- Controller/MapController.php
+++ Controller/MapController.php
@@ -9,22 +9,55 @@ use Gansky\MapBundle\Entity\Point;
 use JMS\DiExtraBundle\Annotation as DI;
 
 /**
-* @Route("/map")
-*/
-class MapController extends Controller
-{
+ * @Route("/map")
+ */
+class MapController extends Controller {
 
     /**
-    * @DI\Inject("doctrine.orm.entity_manager")
-    */
+     * @DI\Inject("doctrine.orm.entity_manager")
+     */
     private $em;
 
     /**
-     * @Route("/index")
+     * @Route("/index", name="index_action")
      * @Template()
      */
-    public function indexAction()
-    {
-      return array();
+    public function indexAction() {
+        return array();
     }
+
+    /**
+     * Controller for getting bounds by zoom and corner coordinates.
+     * North East - right top corner;
+     * South West - left bottom corner;
+     * Latitude - horisontal;
+     * Longitude - vertical;
+     * @Route("/get-bounds", name="get_bounds_action")
+     */
+    public function getBoundsAction() {
+        $zoom = $this->getRequest()->get('zoom');
+        $south_west_latitude = $this->getRequest()->get('south_west_latitude');
+        $south_west_longitude = $this->getRequest()->get('south_west_longitude');
+        $north_east_latitude = $this->getRequest()->get('north_east_latitude');
+        $north_east_longitude = $this->getRequest()->get('north_east_longitude');
+
+        $bounds = $this->em->getRepository('GanskyMapBundle:Point')
+                ->createQueryBuilder('p')
+                ->innerJoin('p.waySet', 'ws')
+                ->innerJoin('ws.way', 'w')
+                ->andWhere('w.level = :way_level')
+                ->andWhere('p.latitude > :south_west_latitude OR p.latitude < :north_east_latitude')
+                ->andWhere('p.longitude > :south_west_longitude OR p.longitude < :north_east_longitude')
+                ->setParameters(array(
+                    'way_level' => $zoom,
+                    'south_west_latitude' => $south_west_latitude,
+                    'north_east_latitude' => $north_east_latitude,
+                    'south_west_longitude' => $south_west_longitude,
+                    'north_east_longitude' => $north_east_longitude))
+                ->getQuery()
+                ->getResult();
+        
+        return new \Symfony\Component\HttpFoundation\JsonResponse($bounds);
+    }
+
 }
